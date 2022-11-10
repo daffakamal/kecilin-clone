94c8416c943c5506490cf0ebbf494c8f2e7dbdca
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-10 17:10:28 +0700

    add assets and components folder

---
+++ components/Home/landingpage.js
@@ -0,0 +1,10 @@
+
+import HeroImg from "../assets/Images/Home/hero-slide-1.png";
+
+export default function LandingPage() {
+    return (
+        <div className="w-full">
+
+        </div>
+    );
+}
