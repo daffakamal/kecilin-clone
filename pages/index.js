08079db0020d01bcc5733286593b6dc97c6106ed
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-11 16:41:25 +0700

    fix(index): improve Head tag

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ export default function Home() {
     <>
       <Head>
         <title>Kecilin</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="kecilin, file compressor, compress file, compression technology provider" />
+        <meta name="description" content="Kecilin is the first and the only Indonesia's cloud and compression technology provider. Making it possible for every business to experience small-is-better." />
+        <link rel="icon" href="/kecilin-logo.svg" />
       </Head>
       <Navbar />
       <LandingPage />
