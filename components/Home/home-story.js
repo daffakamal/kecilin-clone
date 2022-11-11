61eec8060c8e33abaa498669e8e689d0bb8e942f
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-11 22:47:50 +0700

    fix(Home): add youtube video

--- components/Home/home-story.js
+++ components/Home/home-story.js
@@ -3,8 +3,8 @@ export default function HomeStory() {
     return (
         <div className="w-full h-full bg-white">
             <div className="w-full grid px-[24px] sm:px-16 lg:px-32 xl:px-52 2xl:px-80 pt-5">
-                <div className="flex">
-                    <div className="rounded px-6 py-8 text-white bg-gradient-to-r from-cyan-600 to-blue-900 z-10">
+                <div className="grid xs:flex items-center justify-center">
+                    <div className="rounded-lg px-6 py-8 text-white bg-gradient-to-r from-cyan-600 to-blue-900 z-10">
                         <h2 className="font-PoppinsSemi text-[28px] pb-3">
                             The story of how we grew and developed until now
                         </h2>
@@ -12,18 +12,15 @@ export default function HomeStory() {
                             Now in 2022, we are GO-TO-Market ready with many enterprise level solution center arround our proprietary compression algorithm.
                         </p>
                     </div>
-                    <video
-                        autoplay
-                        loop
-                        muted
-                        className="w-full 2xl:w-auto h-auto z-40"
-                    >
-                        <source
-                            src="/assets/Video/video-story.mp4"
-                            type="video/mp4"
-                        />
-                            Your browser does not support the video tag.
-                    </video>
+                    <div className="grid content-center m-auto w-full pt-6 xs:pt-0">
+                        <iframe
+                            className="w-full h-auto aspect-video -translate-x-2 z-40"
+                            src="https://www.youtube.com/embed/fSBPHITgoGw?autoplay=1"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
                 </div>
             </div>
         </div>
