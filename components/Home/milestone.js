91ad696aae24e57f2369279824ca03c47d7ce981
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-26 12:20:56 +0700

    fix(Home): milestone section responsiveness

--- components/Home/milestone.js
+++ components/Home/milestone.js
@@ -8,24 +8,24 @@ export default function Milestone() {
     return (
         <div className="w-full h-full bg-[#f9fafb]">
             <div className="w-full px-[24px] sm:px-16 lg:px-32 xl:px-52 2xl:px-80 py-[110px] 2xl:container 2xl:mx-auto">
-                <div className="grid justify-items-start px-5 py-10">
+                <div className="grid justify-items-start px-2 md:px-5 py-10">
                     <div 
-                        className="flex md:flex gap-x-6" 
+                        className="grid md:flex gap-x-10 3xl:gap-x-6" 
                         data-aos="fade-up" 
                         data-aos-easing="ease"
                         data-aos-duration="400"
                         data-aos-delay="0"
                     >
-                        <h1 className="w-full font-PoppinsSemi text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900 text-[40px]">
+                        <h1 className="w-full md:w-2/3 font-PoppinsSemi text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900 text-[28px] xs:text-[32px] xl:text-[40px]">
                             {"Let's"} Get to Know Kecilin!
                         </h1>
-                        <p className="font-PoppinsLight text-[20px] text-[#6D6D6D]">
+                        <p className="w-full font-PoppinsLight text-[16px] xs:text-[20px] text-[#6D6D6D] pt-6 md:pt-0">
                             Kecilin was founded in 2018 with a mission to create equality in information access even with existing limited telecommunication data access, 
                             we provide a compression technology {"that's"} smooth and efficient.
                         </p>
                     </div>
                     <h1 
-                        className="font-PoppinsSemi text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900 text-[32px] pt-20 pb-6"
+                        className="font-PoppinsSemi text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900 text-[26px] sm:text-[30px] lg:text-[32px] pt-20 pb-6"
                         data-aos="fade-up" 
                         data-aos-easing="ease"
                         data-aos-duration="400"
@@ -34,27 +34,27 @@ export default function Milestone() {
                         Kecilin Milestone
                     </h1>
                     <div 
-                        className="w-full flex flex-wrap md:flex-nowrap justify-between flex-rows"
+                        className="w-full flex flex-wrap md:flex-nowrap justify-between flex-rows gap-y-8 md:gap-y-0"
                         data-aos="fade-up" 
                         data-aos-easing="ease"
                         data-aos-duration="400"
                         data-aos-delay="0"
                     >
-                        <div className="flex flex-col w-full ">
+                        <div className="flex flex-col w-full">
                             <div className="flex items-center gap-x-6">
                                 <Image
                                     src={FileIcon.src}
-                                    className="pointer-events-none"
+                                    className="w-14 sm:w-16 xl:w-auto pointer-events-none"
                                     width={80}
                                     height={80}
                                     loading="lazy"
                                     alt="File icon"
                                 />
-                                <h1 className="font-PoppinsBold text-[36px] text-[#3D3D3D]">
+                                <h1 className="font-PoppinsBold text-[28px] sm:text-[30px] xl:text-[36px] text-[#3D3D3D]">
                                     2.96 PB
                                 </h1>
                             </div>
-                            <p className="w-full pl-2 translate-x-24 font-PoppinsRegular text-[18px] text-[#6D6D6D]">
+                            <p className="w-full sm:pl-2 translate-x-20 xl:translate-x-24 font-PoppinsRegular text-[16px]sm:text-[18px] text-[#6D6D6D]">
                                 Inbound File Size
                             </p>
                         </div>
@@ -62,17 +62,17 @@ export default function Milestone() {
                             <div className="flex items-center gap-x-6">
                                 <Image
                                     src={CloudIcon.src}
-                                    className="pointer-events-none"
+                                    className="w-14 sm:w-16 xl:w-auto pointer-events-none"
                                     width={80}
                                     height={80}
                                     loading="lazy"
                                     alt="Cloud icon"
                                 />
-                                <h1 className="font-PoppinsBold text-[36px] text-[#3D3D3D]">
+                                <h1 className="font-PoppinsBold text-[28px] sm:text-[30px] xl:text-[36px] text-[#3D3D3D]">
                                     988.13 TB
                                 </h1>
                             </div>
-                            <p className="w-full pl-2 translate-x-24 font-PoppinsRegular text-[18px] text-[#6D6D6D]">
+                            <p className="w-full sm:pl-2 translate-x-20 xl:translate-x-24 font-PoppinsRegular text-[16px]sm:text-[18px] text-[#6D6D6D]">
                                 Outbound File Size
                             </p>
                         </div>
@@ -80,17 +80,17 @@ export default function Milestone() {
                             <div className="flex items-center gap-x-6">
                                 <Image
                                     src={SavingStrorageIcon.src}
-                                    className="pointer-events-none"
+                                    className="w-14 sm:w-16 xl:w-auto  pointer-events-none"
                                     width={80}
                                     height={80}
                                     loading="lazy"
                                     alt="Saving storage icon"
                                 />
-                                <h1 className="font-PoppinsBold text-[36px] text-[#3D3D3D]">
+                                <h1 className="font-PoppinsBold text-[28px] sm:text-[30px] xl:text-[36px] text-[#3D3D3D]">
                                     67.41 %
                                 </h1>
                             </div>
-                            <p className="w-full pl-2 translate-x-24 font-PoppinsRegular text-[18px] text-[#6D6D6D]">
+                            <p className="w-full sm:pl-2 translate-x-20 xl:translate-x-24 font-PoppinsRegular text-[16px]sm:text-[18px] text-[#6D6D6D]">
                                 Saving Percentage
                             </p>
                         </div>
