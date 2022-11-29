5c84d202953846220f739e5ebff34a9062805acd
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-29 16:43:13 +0700

    feat(data): add dataTeams

---
+++ components/data/dataTeams.js
@@ -0,0 +1,39 @@
+const teamsList = [
+  {
+    name: "Christoper Farrel Millenio Kusuma",
+    img: "/images/Home/profil1.png",
+    position: "CEO & Founder of KECILIN.ID",
+  },
+  {
+    name: "Bisma Manda Samsu",
+    img: "/images/Home/profil2.png",
+    position: [
+      {
+        main: "CCO & Co Founder Kecilin.id",
+        secondary: "Previously worked at Mandiri Capital Indonesia as SEVP Business Synergy",
+      },
+    ],
+  },
+  {
+    name: "Alexander Rusli",
+    img: "/images/Home/profil3.png",
+    position: "Former President Director & Chief Executive Officer in Indosat Ooredoo",
+  },
+  {
+    name: "Monovan Sakti Jaya Kusuma",
+    img: "/images/Home/profil4.png",
+    position: "Former Engineering Development of Singapore Telecom (SingTel)",
+  },
+  {
+    name: "Toto Sugiri",
+    img: "/images/Home/profil5.png",
+    position: "Founder of Sigma & DCI",
+  },
+  {
+    name: "Hadi Wenas",
+    img: "/images/Home/profil6.png",
+    position: "Former Chief Executive Officer at MatahariMall.com",
+  }
+];
+
+export { sosmedList };
