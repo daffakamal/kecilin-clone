a4f55e199d9d58a8927256770c7ddc58359e79e8
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-12-03 10:13:45 +0700

    feat(Home): add aos on bottom-banner section

--- components/Home/bottom-banner.js
+++ components/Home/bottom-banner.js
@@ -6,7 +6,13 @@ export default function BottomBanner() {
   return (
     <div className="w-full bg-bottom-banner bg-cover">
       <div className="mx-auto relative w-10/12 xl:w-3/4 py-[150px] text-white">
-        <h2 className="flex flex-wrap items-center font-PoppinsBold text-[26px] xs:text-[36px] md:text-[64px] drop-shadow-text-banner mb-[30px]">
+        <h2
+          className="flex flex-wrap items-center font-PoppinsBold text-[26px] xs:text-[36px] md:text-[64px] drop-shadow-text-banner mb-[30px]"
+          data-aos="fade-up"
+          data-aos-easing="ease"
+          data-aos-duration="400"
+          data-aos-delay="0"
+        >
           Ma
           <Image
             src={IconKecilinSmall.src}
@@ -18,10 +24,22 @@ export default function BottomBanner() {
           />
           e a bigger space!
         </h2>
-        <h4 className="font-PoppinsMedium text-[16px] xs:text-[20px] md:text-[28px] mb-[30px]">
+        <h4
+          className="font-PoppinsMedium text-[16px] xs:text-[20px] md:text-[28px] mb-[30px]"
+          data-aos="fade-up"
+          data-aos-easing="ease"
+          data-aos-duration="400"
+          data-aos-delay="100"
+        >
           An inclusive future means protecting the planet we share
         </h4>
-        <p className="font-PoppinsRegular text-[11px] xs:text-[12px] md:text-[14px]">
+        <p
+          className="font-PoppinsRegular text-[11px] xs:text-[12px] md:text-[14px]"
+          data-aos="fade-up"
+          data-aos-easing="ease"
+          data-aos-duration="400"
+          data-aos-delay="100"
+        >
           We believe that in every business, digital data plays an essential
           role in our money-making. Drowned with text-based files, images,
           video, streaming services, and documents every single minute of our
