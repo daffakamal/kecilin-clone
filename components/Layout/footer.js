9ea58fa15bd7f672cef127575f7e78b422a1c83b
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-12-04 15:24:37 +0700

    fix(Layout): adjust responsive footer

--- components/Layout/footer.js
+++ components/Layout/footer.js
@@ -11,17 +11,17 @@ export default function Footer() {
   return (
     <div className="w-full h-full mx-auto bg-white">
       <div className="w-10/12 xl:w-3/4 mx-auto pt-[80px]">
-        <div className="grid grid-cols-3 text-left gap-x-[30px] pb-[30px] border-b-[1px] border-[#B4B4B4] text-[#4A4A4A]">
+        <div className="grid md:grid-cols-3 text-left md:gap-x-[30px] gap-y-[40px] md:gap-y-[40px] pb-[30px] border-b-[1px] border-[#B4B4B4] text-[#4A4A4A]">
           <div className="grid gap-y-[20px]">
             <Image
               src={KecilinLogo.src}
-              className="my-auto pt-2 pointer-events-none"
+              className="w-12 md:w-14 lg:w-14 pointer-events-none"
               width={54}
               height={54}
               loading="lazy"
               alt="Kecilin logo"
             />
-            <h1 className="font-PoppinsRegular text-[11px] xs:text-[12px] md:text-[14px]">
+            <h1 className="font-PoppinsRegular text-[14px]">
               File size can become smaller without decreasing its quality
             </h1>
             <div className="flex flex-row gap-x-6">
@@ -48,14 +48,12 @@ export default function Footer() {
             </div>
           </div>
           <div className="grid gap-y-[20px]">
-            <p className="font-PoppinsBold text-[12px] xs:text-[16px] md:text-[18px]">
+            <p className="font-PoppinsBold text-[16px] md:text-[18px]">
               Jakarta Office
             </p>
-            <div className="font-PoppinsRegular text-[11px] xs:text-[12px] md:text-[14px]">
-              <p>
-                PT MILLENIO AMERTA DATA
-              </p>
-              <br/>
+            <div className="font-PoppinsRegular text-[14px]">
+              <p>PT MILLENIO AMERTA DATA</p>
+              <br />
               <p>
                 Wisma Taranis, Jl. Bendungan Hilir Raya, No. 76, Kel. Bendungan
                 Hilir, Kec. Tanah Abang, Kota Jakarta Pusat, Prov. DKI Jakarta,
@@ -64,10 +62,10 @@ export default function Footer() {
             </div>
           </div>
           <div className="grid gap-y-[20px]">
-            <p className="font-PoppinsBold text-[12px] xs:text-[16px] md:text-[18px]">
+            <p className="font-PoppinsBold text-[16px] md:text-[18px]">
               Contact Us
             </p>
-            <div className="flex items-center gap-x-3 font-PoppinsBold text-[11px] xs:text-[12px] md:text-[14px]">
+            <div className="flex items-center gap-x-3 font-PoppinsBold text-[14px]">
               <Image
                 src={IconTelephone.src}
                 className="pointer-events-none"
@@ -76,14 +74,12 @@ export default function Footer() {
                 loading="lazy"
                 alt="Telephone icon"
               />
-              <p>
-                Admin Kecilin - 081255559133
-              </p>
+              <p>Admin Kecilin - 081255559133</p>
             </div>
-            <p className="font-PoppinsRegular text-[11px] xs:text-[12px] md:text-[14px]">
+            <p className="font-PoppinsRegular text-[14px]">
               Interested to our product?
             </p>
-            <div className="flex items-center gap-x-3 font-PoppinsBold text-[11px] xs:text-[12px] md:text-[14px]">
+            <div className="flex items-center gap-x-3 font-PoppinsBold text-[14px]">
               <Image
                 src={IconMail.src}
                 className="pointer-events-none"
@@ -92,13 +88,11 @@ export default function Footer() {
                 loading="lazy"
                 alt="Mail icon"
               />
-              <p>
-                solution@kecilin.id
-              </p>
+              <p>solution@kecilin.id</p>
             </div>
           </div>
         </div>
-        <div className="flex gap-x-8 pt-[30px] pb-[80px] font-PoppinsSemi text-[#4A4A4A] text-[9px] xs:text-[10px] md:text-[12px]">
+        <div className="flex gap-x-8 pt-[30px] pb-[80px] font-PoppinsSemi text-[#4A4A4A] text-[12px]">
           <a
             href="#!"
             className="hover:underline hover:decoration-[#4A4A4A] cursor-pointer"
@@ -114,7 +108,7 @@ export default function Footer() {
         </div>
       </div>
       <div className="bg-[#F2F2F2] grid justify-items-center py-[40px]">
-        <span className="font-PoppinsRegular text-[#4A4A4A] text-[12px] xs:text-[14px] md:text-[16px]">
+        <span className="font-PoppinsRegular text-[#4A4A4A] text-[14px] md:text-[16px]">
           @2022 Kecilin Systems, Inc.
         </span>
       </div>
