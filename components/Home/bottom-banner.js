2ab3e199b378b51eff7e764dc43bfe8b7e8ecc26
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-12-05 13:23:49 +0700

    fix(Home): bottom-banner section responsive (pt, py, width, font-size)

--- components/Home/bottom-banner.js
+++ components/Home/bottom-banner.js
@@ -5,9 +5,9 @@ import IconKecilinSmall from "../../assets/Images/Home/ic-kecilin-span.svg";
 export default function BottomBanner() {
   return (
     <div className="w-full bg-bottom-banner bg-cover">
-      <div className="mx-auto relative w-10/12 xl:w-3/4 py-[150px] text-white">
+      <div className="mx-auto relative w-10/12 xl:w-3/4 py-[80px] xs:py-[80px] md:py-[100px] lg:py-[150px] text-white">
         <h2
-          className="flex flex-wrap items-center font-PoppinsBold text-[26px] xs:text-[36px] md:text-[64px] drop-shadow-text-banner mb-[30px]"
+          className="flex flex-wrap items-center font-PoppinsBold text-[26px] xs:text-[36px] md:text-[42px] lg:text-[55px] xl:text-[64px] drop-shadow-text-banner mb-[30px]"
           data-aos="fade-up"
           data-aos-easing="ease"
           data-aos-duration="400"
@@ -16,7 +16,7 @@ export default function BottomBanner() {
           Ma
           <Image
             src={IconKecilinSmall.src}
-            className="my-auto pt-2 pointer-events-none"
+            className="w-[20px] xs:w-6 md:w-7 lg:w-10 my-auto xs:pt-1 md:pt-1 pointer-events-none"
             width={35}
             height={35}
             loading="lazy"
@@ -25,7 +25,7 @@ export default function BottomBanner() {
           e a bigger space!
         </h2>
         <h4
-          className="font-PoppinsMedium text-[16px] xs:text-[20px] md:text-[28px] mb-[30px]"
+          className="font-PoppinsMedium text-[16px] xs:text-[20px] md:text-[22px] lg:text-[24px] xl:text-[28px] mb-[30px]"
           data-aos="fade-up"
           data-aos-easing="ease"
           data-aos-duration="400"
@@ -34,7 +34,7 @@ export default function BottomBanner() {
           An inclusive future means protecting the planet we share
         </h4>
         <p
-          className="font-PoppinsRegular text-[11px] xs:text-[12px] md:text-[14px]"
+          className="font-PoppinsRegular text-[12px] md:text-[14px]"
           data-aos="fade-up"
           data-aos-easing="ease"
           data-aos-duration="400"
