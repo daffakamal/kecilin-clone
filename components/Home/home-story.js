79527535073ddd31919fc176f7bc615cd79aa101
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-12-05 19:23:09 +0700

    fix(Home): home-story section responsive (font-size, width)

--- components/Home/home-story.js
+++ components/Home/home-story.js
@@ -1,35 +1,37 @@
 
 export default function HomeStory() {
     return (
-        <div className="w-full h-full bg-white">
-            <div className="w-full grid px-[24px] sm:px-16 lg:px-32 xl:px-52 2xl:px-80 pt-5">
-                <div 
-                    className="grid md:flex items-center justify-center"
-                    data-aos="fade-up" 
-                    data-aos-easing="ease"
-                    data-aos-duration="400"
-                    data-aos-delay="0"
-                >
-                    <div className="rounded-lg px-6 py-8 text-white bg-gradient-to-r from-cyan-600 to-blue-900 z-10">
-                        <h2 className="font-PoppinsSemi text-[28px] pb-3">
-                            The story of how we grew and developed until now
-                        </h2>
-                        <p className="font-PoppinsRegular text-[16px]">
-                            Now in 2022, we are GO-TO-Market ready with many enterprise level solution center arround our proprietary compression algorithm.
-                        </p>
-                    </div>
-                    <div className="grid content-center m-auto w-full pt-6 md:pt-0">
-                        <iframe
-                            className="w-full h-auto aspect-video -translate-x-2 z-40"
-                            src="https://www.youtube.com/embed/fSBPHITgoGw?autoplay=1"
-                            title="YouTube video player"
-                            loading="lazy"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                    </div>
-                </div>
+      <div className="w-full h-full bg-white">
+        <div className="w-full grid px-[24px] sm:px-16 lg:px-32 xl:px-52 2xl:px-80 pt-5">
+          <div
+            className="grid md:flex items-center justify-center"
+            data-aos="fade-up"
+            data-aos-easing="ease"
+            data-aos-duration="400"
+            data-aos-delay="0"
+          >
+            <div className="rounded-lg px-6 py-8 text-white bg-gradient-to-r from-cyan-600 to-blue-900 z-10">
+              <h2 className="font-PoppinsSemi text-[22px] sm:text-[24px] lg:text-[28px] pb-3">
+                The story of how we grew and developed until now
+              </h2>
+              <p className="font-PoppinsRegular text-[14px] lg:text-[16px]">
+                Now in 2022, we are GO-TO-Market ready with many enterprise
+                level solution center arround our proprietary compression
+                algorithm.
+              </p>
             </div>
+            <div className="grid content-center mx-auto w-11/12 md:w-full pt-6 md:pt-0">
+              <iframe
+                className="w-full h-auto object-cover aspect-video -translate-x-2 z-40"
+                src="https://www.youtube.com/embed/fSBPHITgoGw?autoplay=1"
+                title="YouTube video player"
+                loading="lazy"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
         </div>
+      </div>
     );
 }
