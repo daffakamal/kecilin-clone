82300475639659f078ce27e05c7841af7a6d2f65
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-12-06 11:40:29 +0700

    fix(Home): home-comparison section responsive

--- components/Home/home-comparison.js
+++ components/Home/home-comparison.js
@@ -1,32 +1,33 @@
 export default function HomeComparison() {
-    return (
-        <div className="w-full h-full bg-white">
-            <div className="w-full px-[24px] sm:px-16 lg:px-32 xl:px-52 2xl:px-80 py-28 2xl:container 2xl:mx-auto">
-                <div className="rounded bg-[#dcf1fc] px-5 sm:px-8 md:px-6 py-6 sm:py-8 md:py-7 lg:py-10">
-                    <div className="grid md:flex justify-items-center gap-x-6">
-                        <video
-                            autoplay
-                            loop
-                            muted
-                            className="w-full md:w-7/12 lg:w-8/12 3xl:w-7/12"
-                        >
-                            <source
-                                src="https://kecilin.id/assts/video/example-compress.mp4"
-                                type="video/mp4"
-                            />
-                                Your browser does not support the video tag.
-                        </video>
-                        <div className="relative mx-auto">
-                            <h2 className="font-PoppinsSemi text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900 text-[20px] sm:text-[24px] pt-4 md:pt-0 pb-4">
-                                Compare It Now
-                            </h2>
-                            <p className="font-PoppinsLight text-[14px] text-black">
-                                Reduce video size by 50% while enhancing object colors and textures
-                            </p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="w-full h-full bg-white">
+      <div className="w-full px-[24px] sm:px-16 lg:px-32 xl:px-52 2xl:px-80 py-20 md:py-28 xl:py-28 3xl:py-32 3xl:container 3xl:mx-auto">
+        <div className="rounded bg-[#dcf1fc] px-5 sm:px-8 md:px-6 py-6 sm:py-8 md:py-7 lg:py-10">
+          <div className="grid md:flex justify-items-center gap-x-6">
+            <video
+              autoplay
+              loop
+              muted
+              className="w-full md:w-7/12 lg:w-8/12 2xl:w-[610px] 3xl:w-[680px]"
+            >
+              <source
+                src="https://kecilin.id/assts/video/example-compress.mp4"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+            <div className="relative mx-auto">
+              <h2 className="font-PoppinsSemi text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900 text-[20px] sm:text-[24px] pt-4 md:pt-0 pb-4">
+                Compare It Now
+              </h2>
+              <p className="font-PoppinsLight text-[14px] text-black">
+                Reduce video size by 50% while enhancing object colors and
+                textures
+              </p>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
