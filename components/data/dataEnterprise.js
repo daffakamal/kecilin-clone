e67cb21dfd0ab629565649b0c19bf93a020767d4
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-12-18 03:58:57 +0700

    fix(data): delete img property from enterpriseList

--- components/data/dataEnterprise.js
+++ components/data/dataEnterprise.js
@@ -2,22 +2,18 @@ const enterpriseList = [
   {
     id: "1",
     description: "Slow transfer speed",
-    img: "/images/Home/profil1.png",
   },
   {
     id: "2",
     description: "Full storage",
-    img: "/images/Home/profil1.png",
   },
   {
     id: "3",
     description: "Expensive transfer speed and storage fees",
-    img: "/images/Home/profil1.png",
   },
   {
     id: "4",
     description: "Or even all of them!",
-    img: "/images/Home/profil1.png",
   },
 ];
 
