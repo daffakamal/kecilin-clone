62e140768240043c8f8ed54144f9ca10159df5e9
Daffa K <dvffvkml@gmail.com>
2022-12-22 19:33:30 +0700

    fix(Enterprise): fixing description and max-width

--- components/Enterprise/cctv.js
+++ components/Enterprise/cctv.js
@@ -11,7 +11,7 @@ export default function CCTV() {
           The Implementation of Kecilin's Compression Technology
         </h1>
         <div className="grid justify-items-center lg:flex justify-center mx-auto gap-x-[2rem] xl:gap-x-[3rem] 2xl:gap-x-[4rem] gap-y-[3rem] pt-20">
-          <div className="w-full grid max-w-[280px] sm:max-w-[360px] lg:max-w-[250px]">
+          <div className="w-full grid max-w-[280px] sm:max-w-[360px] lg:max-w-[250px] xl:max-w-[290px]">
             <Image
               src={CCTVImplementImg.src}
               className="mx-auto w-full pointer-events-none"
@@ -28,13 +28,13 @@ export default function CCTV() {
           </div>
           <Image
             src={Arrow.src}
-            className="mx-auto rotate-90 lg:rotate-0 py-6 lg:py-0 pointer-events-none"
+            className="w-16 mx-auto rotate-90 lg:rotate-0 py-6 lg:py-0 pointer-events-none"
             width={82}
             height={18}
             loading="lazy"
             alt="CCTV implementation using Kecilin's compression technology"
           />
-          <div className="w-full grid max-w-[280px] sm:max-w-[360px] lg:max-w-[250px]">
+          <div className="w-full grid max-w-[280px] sm:max-w-[360px] lg:max-w-[250px] xl:max-w-[290px]">
             <Image
               src={CCTVImplementImg.src}
               className="mx-auto w-full pointer-events-none"
@@ -44,9 +44,9 @@ export default function CCTV() {
               alt="CCTV implementation using Kecilin's compression technology"
             />
             <div className="grid justify-items-start bg-[#dcf1fc] p-[1.5rem] gap-y-[1rem] font-PoppinsLight text-[#2F3D4D] text-[18px]">
-              <span>Original Video :</span>
-              <h1 className="font-PoppinsSemi text-[32px]">357 MB</h1>
-              <p>8 mbps, 20 fps, 15 Minutes</p>
+              <span>Compressed Video :</span>
+              <h1 className="font-PoppinsSemi text-[32px]">26 MB</h1>
+              <p>0,4 mbps, 20 fps, 15 Minutes</p>
             </div>
           </div>
           <div className="w-auto grid text-center lg:text-left content-start gap-y-6">
