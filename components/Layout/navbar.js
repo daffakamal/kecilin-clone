a48fde04db9f8aedef4e43678baa827f4fb0a413
unknown <daffa.kamal@mail.ugm.ac.id>
2022-12-27 10:16:46 +0700

    fix: change font-color for compress now button

--- components/Layout/navbar.js
+++ components/Layout/navbar.js
@@ -215,7 +215,7 @@ function Navbar() {
           </div>
           <div className="flex justify-end pt-1 pb-3">
             <Link href="/#compress-page">
-              <button className="font-PoppinsMedium uppercase px-4 py-2 bg-gradient-to-r from-cyan-600 to-blue-900 rounded-full text-[11px] cursor-pointer">
+              <button className="font-PoppinsMedium text-white uppercase px-4 py-2 bg-gradient-to-r from-cyan-600 to-blue-900 rounded-full text-[11px] cursor-pointer">
                 Compress now
               </button>
             </Link>
@@ -250,7 +250,7 @@ function Navbar() {
           </div>
           <div className="flex justify-end py-3">
             <Link href="/#compress-page">
-              <button className="font-PoppinsMedium uppercase px-5 py-2 bg-gradient-to-r from-cyan-600 to-blue-900 rounded-full text-[12px] cursor-pointer">
+              <button className="font-PoppinsMedium text-white uppercase px-5 py-2 bg-gradient-to-r from-cyan-600 to-blue-900 rounded-full text-[12px] cursor-pointer">
                 Compress now
               </button>
             </Link>
