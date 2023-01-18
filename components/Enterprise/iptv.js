02b429b8013a59bd18e5421a074012c2567774c2
Daffa K <dvffvkml@gmail.com>
2023-01-19 00:01:31 +0700

    fix(Enterprise): fixing failed deployment

--- components/Enterprise/iptv.js
+++ components/Enterprise/iptv.js
@@ -8,7 +8,7 @@ export default function IPTV() {
     <div className="w-full h-full bg-[#EAF7FE] px-[24px] sm:px-16 lg:px-32 xl:px-52 2xl:px-80 py-12 sm:py-20 lg:py-24 xl:py-32 gap-y-[6rem]">
       <div className="mx-auto container">
         <h1 className="font-PoppinsSemi text-center text-[26px] sm:text-[28px] lg:text-[32px] xl:text-[32px] text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900">
-          The Implementation of Kecilin's Compression Technology
+          The Implementation of {"Kecilin's"} Compression Technology
         </h1>
         <div className="grid justify-items-center lg:flex justify-center mx-auto gap-x-[2rem] xl:gap-x-[3rem] 2xl:gap-x-[4rem] gap-y-[3rem] pt-20">
           <div className="w-auto grid text-left lg:text-left content-start gap-y-6">
