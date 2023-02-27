feb9da5ff2a6c470653f2a6f01b53496c9261891
Daffa K <dvffvkml@gmail.com>
2023-02-27 19:50:14 +0700

    fix(Home): update responsiveness on milestone section

--- components/Home/milestone.js
+++ components/Home/milestone.js
@@ -4,23 +4,23 @@ import CloudIcon from "../../assets/Images/Home/icon-milestone2.svg";
 import SavingStrorageIcon from "../../assets/Images/Home/icon-milestone3.svg";
 
 const styleDescIcon =
-  "w-full sm:pl-2 xl:pl-4 2xl:pl-6 translate-x-20 xl:translate-x-16 font-PoppinsRegular text-[16px] sm:text-[18px] text-[#6D6D6D]";
+  "w-fit sm:pl-2 xl:pl-4 2xl:pl-6 translate-x-20 xl:translate-x-16 font-PoppinsRegular text-[16px] sm:text-[18px] text-[#6D6D6D]";
 
 export default function Milestone() {
     return (
       <div className="w-full h-full bg-[#f9fafb]">
         <div className="w-full px-[24px] sm:px-16 py-[110px]">
-          <div className="grid justify-items-start px-2 xl:px-5 py-10 large:max-w-[1100px] mx-auto">
+          <div className="grid justify-items-start px-2 xl:px-5 lg:py-6 large:max-w-[1100px] mx-auto">
             <div
-              className="grid md:flex gap-x-10 3xl:gap-x-6"
+              className="grid justify-items-center md:flex gap-x-10 3xl:gap-x-6"
               data-aos="fade-up"
               data-aos-easing="ease"
               data-aos-duration="400"
               data-aos-delay="0">
-              <h1 className="w-full md:w-2/3 font-PoppinsSemi text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900 text-[28px] xs:text-[32px] xl:text-[40px]">
+              <h1 className="w-full md:w-2/3 font-PoppinsSemi text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900 text-[28px] xs:text-[32px] xl:text-[40px] text-center md:text-left">
                 {"Let's"} Get to Know Kecilin!
               </h1>
-              <p className="w-full font-PoppinsLight text-[16px] xs:text-[20px] text-[#6D6D6D] pt-6 md:pt-0">
+              <p className="w-full font-PoppinsLight text-[16px] xs:text-[20px] text-[#6D6D6D] pt-6 md:pt-0 text-justify">
                 Kecilin was founded in 2018 with a mission to create equality in
                 information access even with existing limited telecommunication
                 data access, we provide a compression technology {"that's"}{" "}
@@ -36,12 +36,12 @@ export default function Milestone() {
               Kecilin Milestone
             </h1>
             <div
-              className="w-full flex flex-wrap md:flex-nowrap justify-between flex-rows gap-y-8 md:gap-y-0"
+              className="w-full flex flex-wrap lg:flex-nowrap justify-between gap-y-8 md:gap-y-7 gap-x-10 md:gap-x-0"
               data-aos="fade-up"
               data-aos-easing="ease"
               data-aos-duration="400"
               data-aos-delay="0">
-              <div className="flex flex-col w-full">
+              <div className="flex flex-col min-w-[260px]">
                 <div className="flex items-center gap-x-6">
                   <Image
                     src={FileIcon.src}
@@ -57,7 +57,7 @@ export default function Milestone() {
                 </div>
                 <p className={styleDescIcon}>Inbound File Size</p>
               </div>
-              <div className="w-full flex flex-col">
+              <div className="w-full flex flex-col max-w-[260px]">
                 <div className="flex items-center gap-x-6">
                   <Image
                     src={CloudIcon.src}
@@ -73,7 +73,7 @@ export default function Milestone() {
                 </div>
                 <p className={styleDescIcon}>Outbound File Size</p>
               </div>
-              <div className="w-full flex flex-col">
+              <div className="w-full flex flex-col max-w-[260px]">
                 <div className="flex items-center gap-x-6">
                   <Image
                     src={SavingStrorageIcon.src}
