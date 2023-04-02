9a8137f7c4f9629dbd11a5f4bdde28bcf73d62e1
wannabegooddevv <daffa.kamal@mail.ugm.ac.id>
2023-04-02 15:47:02 +0700

    fix(Enterprise): add min-height in title section

--- components/Enterprise/title.js
+++ components/Enterprise/title.js
@@ -1,16 +1,15 @@
 import Image from "next/image";
-
 import TitleImg from "../../assets/Images/Enterprise/enterprise-header.png";
 import ChecklistImg from "../../assets/Images/checklist.svg";
 import { enterpriseList } from "../data/dataEnterprise";
 
 export default function Title() {
   return (
-    <div className="w-full h-full bg-white px-[24px] sm:px-16 pt-0 md:pt-16 pb-12 sm:pb-14 md:pb-16 g:py-24 xl:py-32">
+    <div className="w-full grid content-center min-h-full md:min-h-[30vh] lg:min-h-[22vh] 2xl:min-h-[50vh] bg-white px-[24px] sm:px-16 pt-0 md:pt-16 pb-12 sm:pb-14 md:pb-16 g:py-24 xl:py-32">
       <div className="mx-auto large:max-w-[1100px] md:flex items-center gap-x-10 lg:gap-x-[4rem]">
         <Image
           src={TitleImg.src}
-          className="mx-auto w-[300px] xs:w-[340px] md:w-[300px] lg:w-[370px] xl:w-[450px] 3xl:w-auto pointer-events-none"
+          className="mx-auto w-[300px] xs:w-[340px] md:w-[300px] lg:w-[480px] xl:w-[500px] 3xl:w-auto pointer-events-none"
           width={554}
           height={554}
           loading="lazy"
@@ -20,18 +19,18 @@ export default function Title() {
           <h1 className="font-PoppinsSemi text-[26px] sm:text-[28px] lg:text-[32px] xl:text-[36px] text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-900">
             Does Your Enterprise Have Problems Like This?
           </h1>
-          <div className="pt-[2.5rem] md:pt-[3rem] grid gap-y-4">
+          <div className="pt-[2.5rem] lg:pt-[3rem] grid gap-y-4">
             {enterpriseList.map((item) => (
-              <ul key={item.id} className="w-full flex gap-x-3 mx-auto">
+              <ul key={item.id} className="w-full flex gap-x-3 m-auto">
                 <Image
                   src={ChecklistImg.src}
-                  className="my-auto pointer-events-none"
+                  className="w-6 lg:w-7 my-auto pointer-events-none"
                   width={32}
                   height={32}
                   alt="Checklist"
                   loading="lazy"
                 />
-                <li className="w-fit font-PoppinsRegular text-[20px] lg:text-[20px] text-[#494949]">
+                <li className="w-fit font-PoppinsRegular text-[18px] lg:text-[20px] text-[#494949]">
                   {item.description}
                 </li>
               </ul>
